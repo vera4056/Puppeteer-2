03704bf1450ec8c3e2b02e9f97de3df60d98b458
vera4056 <103599166+vera4056@users.noreply.github.com>
2023-07-30 13:27:45 +0300

    tests modified

--- net.test.js
+++ net.test.js
@@ -6,6 +6,7 @@ let page;
 beforeEach(async () => {
   page = await browser.newPage();
   await page.setDefaultNavigationTimeout(0);
+  await page.goto("http://qamid.tmweb.ru/client/index.php");// перейти на сайт 
 });
 
 afterEach(() => {
@@ -13,59 +14,46 @@ afterEach(() => {
 });
 
 describe("qamid.tmweb.ru tests", () => {
-  beforeEach(async () => {
-    page = await browser.newPage();
-    await page.goto("http://qamid.tmweb.ru/client/index.php");// перейти на сайт 
+  test("Should book for a ticket", async () => {
+    const dayPage = await page.$("a.page-nav__day:nth-child(2)"); // перейти на скнопку дата -конкретный день
+    await dayPage.click(); // кликаем и выбираем конкретный день 
+    const seanceTime = await page.$("a.movie-seances__time"); // перейти на кнопку время по указанному селектору
+    await seanceTime.click();
+    const chairBuying = await page.$("span.buying-scheme__chair.buying-scheme__chair_standart:nth-child(8)"); // попадаем на страницу бронирования и находим селектор со свободным местом 
+    await clickElement(page, chairBuying);
+    await clickElement(page, "button.acceptin-button"); // находит кнопку -забронировать и кликает на нее
+    const getBooking = await page.$("button.acceptin-button");// получить код бронирования
+    await clickElement(page, getBooking);
+    const title = await getText(page, 'h2');
+    expect(title).toContain("ЭЛЕКТРОННЫЙ БИЛЕТ")
   });
 
-  test("Should book for a ticket", async () => {
-    const dayPage = await page.$("a.page-nav__day"); // перейти на скнопку дата -конкретный день
+  test("Should book for two tickets", async () => {
+    const dayPage = await page.$("a.page-nav__day:nth-child(2)"); // перейти на скнопку дата -конкретный день
     await dayPage.click(); // кликаем и выбираем конкретный день 
     const seanceTime = await page.$("a.movie-seances__time"); // перейти на кнопку время по указанному селектору
     await seanceTime.click();
-    const chairBuying = await page.$("div.span.buying-scheme__chair.buying-scheme__chair_standart");// попадаем на страницу бронирования и находим селектор со свободным местом 
-    await chairBuying.click();
-    const booking ="button.acceptin-button"; // селекторы кнопки забронировать
-    await page.waitForSelector(booking, {visible: true, }); // находит кнопку забронировать
-    await booking.click(); // кликаем на кнопку забронировать
-    const getBooking = await page.$eval("body > main > section > button", link => link.textContent); // находим селектор кнопки получить код бронирования
-    await getBooking.click();
-    await page.waitForSelector("body > main > section > header > h2");
-    const title = await page.title();
-    expect(title).toContain("ЭЛЕКТРОННЫЙ БИЛЕТ")
-   });
-
-  test("Should book for a ticket2", async () => {
-    const dayPage = await page.$("a.page-nav__day.page-nav__day_weekend"); // перейти на скнопку дата -конкретный день
-    await dayPage.click(); // кликаем и выбираем конкретный день 
-    const seanceTime = await page.$("a.movie-seances__time"); // перейти на кнопку время по указанному селектору
-    await seanceTime.click();
-    const chairBuying1 = await page.$("div.span.buying-scheme__chair.buying-scheme__chair_standart");// попадаем на страницу бронирования и находим селектор со свободным местом 
-    await chairBuying1.click();
-    const chairBuying2 = await page.$("div.span.buying-scheme__chair.buying-scheme__chair_standart");// попадаем на страницу бронирования и находим селектор со свободным местом 
-    await chairBuying2.click();
-    const booking ="button.acceptin-button"; // селекторы кнопки забронировать
-    await page.waitForSelector(booking, {visible: true, }); // находит кнопку забронировать
-    await booking.click(); // кликаем на кнопку забронировать
-    const actual = await page.$eval("button.acceptin-button", link => link.textContent); // находим селектор кнопки получить код бронирования
-    await actual.click();
-    await page.waitForSelector("h2.ticket__check-title");
-    const title = await page.title();
+    const chairBuying1 = await page.$("div.span.buying-scheme__chair.buying-scheme__chair_vip.div:nth-child(8).span:nth-child(7)"); // попадаем на страницу бронирования и находим селектор со свободным местом 
+    await clickElement(page, chairBuying1);
+    const chairBuying2 = await page.$("div.span.buying-scheme__chair.buying-scheme__chair_standart.div:nth-child(8).span:nth-child(8)"); // попадаем на страницу бронирования и находим селектор со свободным местом 
+    await clickElement(page, chairBuying2);
+    await clickElement(page, "button.acceptin-button");
+    const getBooking = await page.$("button.acceptin-button");// получить код бронирования
+    await clickElement(page, getBooking);
+    const title = await getText(page, 'h2');
     expect(title).toContain("ЭЛЕКТРОННЫЙ БИЛЕТ")
     });
   
   test("Should book for a already booked ticket", async () => {
-    const dayPage = await page.$("a.page-nav__day.page-nav__day_weekend"); // перейти на скнопку дата -конкретный день
+    const dayPage = await page.$("a.page-nav__day:nth-child(2)"); // перейти на скнопку дата -конкретный день
     await dayPage.click(); // кликаем и выбираем конкретный день 
     const seanceTime = await page.$("a.movie-seances__time"); // перейти на кнопку время по указанному селектору
     await seanceTime.click();
-    const chairBuying = await page.$("div.span.buying-scheme__chair buying-scheme__chair_disabled");// попадаем на страницу бронирования и находим селектор с занятым местом, например ряд 7/6 место
-    await chairBuying.click();
-    const booking ="section.buying"; // селекторы кнопки забронировать в неактивном состоянии
-    await page.waitForSelector(booking, {visible: false, }); // находит кнопку забронировать в неактивном состоянии
-    await booking.click(); // кликаем на кнопку забронировать и ничего не происход
-    await page.waitForSelector("h2.buying__info-title");
-    const title = await page.title();
-    expect(title).toContain("Зверополис")
-       });
-    }); 
+    const chairBuying = await page.$("span.buying-scheme__chair.buying-scheme__chair_disabled");// попадаем на страницу бронирования и находим селектор с занятым местом, например ряд 7/6 место
+    await clickElement(page, chairBuying);
+    const getBooking = await page.$("button.acceptin-button");
+    await clickElement(page, getBooking);
+    const error = await page.$("button.acceptin-button"); // тут как бы ничего не происходит, КНОПКА не активна
+    expect(error).toContain(`Selector is not clickable: ${getBooking}`);
+    });
+  }); 
