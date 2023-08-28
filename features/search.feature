7b1ab28dc072918682ca8edd709a2e1359be1b12
vera4056 <103599166+vera4056@users.noreply.github.com>
2023-08-28 21:30:20 +0300

    steps.js modified

--- features/search.feature
+++ features/search.feature
@@ -1,10 +1,10 @@
 Feature: Movie website
     Scenario: Should book a ticket 
         Given user is on "http://qamid.tmweb.ru/client/index.php" page
-        When user chooses "a.page-nav__day:nth-child(3)" day of show 
-        And user chooses "a.movie-seances__time" show time 
+        When user chooses 3 day of show 
+        When user chooses "a.movie-seances__time" show time 
         When user selects seat 3 in the row 3
-        And user clicks ".acceptin-button"
+        And user clicks ".acceptin-button" submit button
         Then users sees a header "Электронный билет"
 
 
--- features/step_definitions/search.steps.js
+++ features/step_definitions/search.steps.js
@@ -23,14 +23,14 @@ Given('user is on {string} page', {timeout: 6000 }, async function (string) {
 When('user chooses {int} day of show', async function (int) { 
   await clickElement(this.page, "a.page-nav__day:nth-child(${int})");
 });
-And('user chooses {string} show time', async function (string) {
+When('user chooses {string} show time', async function (string) {
   await clickElement(this.page, string);
 });
 When('user selects seat {int} in the row {int}', async function (int, int2) {
   await clickElement(this.page, ".buying-scheme__row.buying-scheme__chair:nth-child(${int, int2})");
   });
 And('user clicks {string} submit button', async function (string) {
-    await clickElement(this.page, ".acceptin-button", string);
+    await clickElement(this.page, string);
   });
 Then('users sees a header {string}', async function (string) {
    const actual = await getText(this.page, 'h2', string);
